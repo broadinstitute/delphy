c1fb8a60ead643e6d4e77b823ea5cd837b13479c
Patrick Varilly <pvarilly@broadinstitute.org>
2024-11-29 16:50:04 +0100

    PoC that SDL2 & SDL2_ttf work well on Ubuntu

--- tools/delphy_ui.cpp
+++ tools/delphy_ui.cpp
@@ -1,4 +1,4 @@
-#include <chrono>
+/*#include <chrono>
 #include <iostream>
 
 #ifdef __APPLE__
@@ -935,7 +935,7 @@ auto ui_main_loop(Processed_cmd_line& c) -> int {
 
 }  // namespace delphy
 
-auto main(int argc, char** argv) -> int {
+auto main2(int argc, char** argv) -> int {
   using namespace delphy;
 
   absl::InitializeLog();
@@ -944,4 +944,144 @@ auto main(int argc, char** argv) -> int {
   auto c = process_args(argc, argv);
 
   return ui_main_loop(c);
+} */
+
+#include "SDL.h"
+#include "SDL_ttf.h"
+
+void render_text(
+    SDL_Renderer *renderer,
+    int x,
+    int y,
+    const char *text,
+    TTF_Font *font,
+    SDL_Rect *rect,
+    SDL_Color *color
+) {
+    SDL_Surface *surface;
+    SDL_Texture *texture;
+
+    surface = TTF_RenderText_Solid(font, text, *color);
+    texture = SDL_CreateTextureFromSurface(renderer, surface);
+    rect->x = x;
+    rect->y = y;
+    rect->w = surface->w;
+    rect->h = surface->h;
+    SDL_FreeSurface(surface);
+    SDL_RenderCopy(renderer, texture, NULL, rect);
+    SDL_DestroyTexture(texture);
+}
+
+int main(int /*argc*/, [[maybe_unused]] char *argv[])
+{
+    // returns zero on success else non-zero
+    if (SDL_Init(SDL_INIT_EVERYTHING) != 0) {
+        printf("error initializing SDL: %s\n", SDL_GetError());
+        return 1;
+    }
+    SDL_Window* win = SDL_CreateWindow("GAME", // creates a window
+                                    SDL_WINDOWPOS_CENTERED,
+                                    SDL_WINDOWPOS_CENTERED,
+                                    1000, 1000, 0);
+
+    // triggers the program that controls
+    // your graphics hardware and sets flags
+    Uint32 render_flags = SDL_RENDERER_ACCELERATED;
+
+    // creates a renderer to render our images
+    SDL_Renderer* rend = SDL_CreateRenderer(win, -1, render_flags);
+
+    ///
+    /// Section 2: SDL image loader
+    ///
+
+    ///
+    /// Section 4: SDL ttf and rendering text
+    ///
+    TTF_Init();
+    TTF_Font *font = TTF_OpenFont("/usr/share/fonts/truetype/freefont/FreeSans.ttf", 24);
+    if (font == NULL) {
+        printf("error initializing TTF: %s\n", TTF_GetError());
+        return 1;
+    }
+
+    // controls animation loop
+    int close = 0;
+    
+    // animation loop
+    while (!close) {
+        SDL_Event event;
+
+        // Events management
+        while (SDL_PollEvent(&event)) {
+            switch (event.type) {
+
+            case SDL_QUIT:
+                // handling of close button
+                close = 1;
+                break;
+
+            case SDL_KEYDOWN:
+                // keyboard API for key pressed
+                switch (event.key.keysym.scancode) {
+                case SDL_SCANCODE_ESCAPE:
+                    close = 1;
+                    break;
+                default:
+                    break;
+                }
+            }
+        }
+
+        // clears the screen
+        SDL_RenderClear(rend);
+
+       ///
+       /// Section 4: SDL ttf and rendering text
+       ///
+        // create a rectangle to update with the size of the rendered text
+        SDL_Rect text_rect;
+
+        // The color for the text we will be displaying
+        SDL_Color white = {255, 255, 255, 0};
+
+        // so we can have nice text, two lines one above the next
+        render_text(rend, 10, 10, "Hello World!", font, &text_rect, &white);
+        render_text(rend, 10, text_rect.y + text_rect.h, "Conan demo by JFrog", font, &text_rect, &white);
+        
+        // triggers the double buffers
+        // for multiple rendering
+        SDL_RenderPresent(rend);
+
+        // calculates to 60 fps
+        SDL_Delay(1000 / 60);
+    }
+
+    ///
+    /// Section 3: Game Loop and Basic Controls
+    ///
+
+    ///
+    /// Section 5: Freeing resources
+    ///
+
+    // close font handle
+    TTF_CloseFont(font);
+
+    // close TTF
+    TTF_Quit();
+
+    // destroy renderer
+    SDL_DestroyRenderer(rend);
+
+    // We destroy our window. We are passing in the pointer
+    // that points to the memory allocated by the 
+    // 'SDL_CreateWindow' function. Remember, this is
+    // a 'C-style' API, we don't have destructors.
+    SDL_DestroyWindow(win);
+    
+    // We safely uninitialize SDL2, that is, we are
+    // taking down the subsystems here before we exit
+    // our program.
+    SDL_Quit();
 }
